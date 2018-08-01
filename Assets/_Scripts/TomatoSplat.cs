a2fa53c2a1d575ab5efcf402f27b86ee6fa04a93
Unknown <jabcog@hotmail.com>
2018-08-01 12:01:03 +1200

    Tomato Splat & Phone res Implemented

---
+++ Assets/_Scripts/TomatoSplat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoodleStudio95;
+
+public class TomatoSplat : MonoBehaviour {
+
+	public DoodleAnimator anim;
+	public Transform splatHolder;
+	public GameObject splat;
+	private GameObject splatClone;
+	public DoodleAnimationFile splatAnimation;
+
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		other.gameObject.GetComponentInParent<CharacterMovement> ().GotHit();
+		gameObject.GetComponent<SpriteRenderer>().enabled = false;
+		DoSplat ();
+	}
+		
+
+	IEnumerator PlaySplat() {
+		splatClone = Instantiate (splat, splatHolder.transform);
+		splatClone.transform.parent = splatHolder;
+		DoodleAnimator animator = splatClone.GetComponent<DoodleAnimator>();
+		yield return animator.PlayAndPauseAt();
+		animator.Stop();
+		Destroy (splatClone);
+		Destroy (gameObject);
+		StopCoroutine ("PlaySplat");
+	}
+
+	public void DoSplat() {
+		StartCoroutine(PlaySplat());
+	}
+
+}
