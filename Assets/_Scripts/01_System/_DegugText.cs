c8aeacd6485f7983e5f51001fda2f6623db21c34
Unknown <jabcog@hotmail.com>
2018-09-30 13:49:42 +1200

    Debug text works

---
+++ Assets/_Scripts/01_System/_DegugText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _DegugText : MonoBehaviour {
+
+    public CharacterMovement gbgo;
+
+    public Text groundContact;
+    public Text trueGroundContact;
+
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		if (gbgo._groundContact == true)
+        {
+            groundContact.text = "GroundContact: yes";
+        }
+        else
+        {
+            groundContact.text = "GroundContact: no";
+        }
+
+        if(gbgo.trueGroundContact == true)
+        {
+            trueGroundContact.text = "T_GroundContact: yes";
+        }
+        else
+        {
+            trueGroundContact.text = "T_GroundContact: no";
+        }
+	}
+}
