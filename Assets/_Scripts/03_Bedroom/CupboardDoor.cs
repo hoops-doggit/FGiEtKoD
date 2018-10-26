46ed8034c26394be343b7adc1d87fb698e986c0a
Unknown <jabcog@hotmail.com>
2018-10-26 17:26:00 +1300

    LevelDesign02

--- Assets/_Scripts/03_Bedroom/CupboardDoor.cs
+++ Assets/_Scripts/03_Bedroom/CupboardDoor.cs
@@ -6,24 +6,30 @@ public class CupboardDoor : MonoBehaviour {
 
     public Light light01;
     public Light light02;
+    public Light light03;
 
     private float light01Intensity;
     private float light02Intensity;
+    private float light03Intensity;
 
     public bool collided;
 
     public GameObject rightDoor;
 
     float t = 0.0f;
+    float t3 = 0f;
 
     private void Start()
     {
         light01.gameObject.SetActive(true);
         light02.gameObject.SetActive(true);
+        light03.gameObject.SetActive(true);
         light01Intensity = light01.intensity;
         light02Intensity = light02.intensity;
+        light03Intensity = light03.intensity;
         collided = false;
         t = 0;
+        t3 = 0; 
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,12 +44,14 @@ public class CupboardDoor : MonoBehaviour {
     void FixedUpdate()
     {
         light01.intensity = Mathf.Lerp(light01Intensity, 0f, t);
+        light03.intensity = Mathf.Lerp(light03Intensity, 0f, t3);
         light02.intensity = Mathf.Lerp(0f, light02Intensity, t);
 
         if (collided)
         {
             Debug.Log(t);
             t += 10f * Time.deltaTime;
+            t3 += 20f * Time.deltaTime;
         }
     }
 
