743a9a1950befae3c3a0d2fba41e6d6e6861b3c4
Unknown <jabcog@hotmail.com>
2018-11-02 17:24:23 +1300

    Dustv02

---
+++ Assets/_Scripts/02_Game/ClothesPile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothesPile : MonoBehaviour {
+
+    public ParticleSystem part01;
+    public ParticleSystem part02;
+    public ParticleSystem part03;
+
+    private void Awake()
+    {
+        part01.Pause();
+        part02.Pause();
+        part03.Pause();
+    }
+
+    void EmitParticles()
+    {
+        part01.Play();
+        part02.Play();
+        part03.Play();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        EmitParticles();
+    }
+
+}
