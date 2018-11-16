f75cdd188b0be8b801dac0d27db0bc4e8811eab0
Jacob <jabcog@hotmail.com>
2018-11-16 15:31:15 +1300

    MaximumPush

--- Assets/_Scripts/01_System/BakedAnimator.cs
+++ Assets/_Scripts/01_System/BakedAnimator.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 public class BakedAnimator : MonoBehaviour {
 	
 	public Sprite[] m_Sprites;
+
+
 	public int FramesPerSecond = 8;
 	public float Speed = 1.0f;
     private float sprite;
     private float spriteCounter;
     public CharacterMovement cm;
 
+    public Sprite[] pink;
+    public Sprite[] blue;
+    public Sprite[] green;
+    public Sprite[] orange;
+    public Sprite[] yellow;
 
 
     SpriteRenderer spriteRenderer;
@@ -20,6 +27,31 @@ public class BakedAnimator : MonoBehaviour {
 		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
+    public void ChangeColor(string colour){
+        switch (colour){
+            case "pink":
+                m_Sprites = pink;
+                break;
+            case "blue":
+                m_Sprites = blue;
+                break;
+            case "green":
+                m_Sprites = green;
+                break;
+            case "orange":
+                m_Sprites = orange;
+                break;
+            case "yellow":
+                m_Sprites = yellow;
+                break;
+            default:
+                m_Sprites = pink;
+                break;
+        }
+
+
+    }
+
 	void Update() {
 		if (!spriteRenderer || m_Sprites.Length == 0)
 			return;
@@ -29,7 +61,7 @@ public class BakedAnimator : MonoBehaviour {
 		spriteRenderer.sprite = s;
 
         //For sending to the character to tell which foot is placed
-        float sprite = (int)Mathf.Repeat(t, m_Sprites.Length);
+        sprite = (int)Mathf.Repeat(t, m_Sprites.Length);
 
         if (sprite != spriteCounter)
         {
---
+++ Assets/_Scripts/02_Game/Pickup_Jelly.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Jelly : MonoBehaviour {
+
+    public enum JellyColour
+    {
+        pink,
+        blue,
+        green,
+        orange,
+        yellow
+    }
+
+    public Sprite pink;
+    public Sprite blue;
+    public Sprite green;
+    public Sprite orange;
+    public Sprite yellow;
+
+    public JellyColour jc;
+    private SpriteRenderer _sr;
+
+    private string colorText;
+    private GameObject _gb;
+
+    private void Awake()
+    {
+        _sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        jc = (JellyColour)Random.Range(0, 5);
+
+        switch (jc)
+        {
+            case JellyColour.pink:
+                _sr.sprite = pink;
+                colorText = "pink";
+                break;
+            case JellyColour.blue:
+                _sr.sprite = blue;
+                colorText = "blue";
+                break;
+            case JellyColour.green:
+                _sr.sprite = green;
+                colorText = "green";
+                break;
+            case JellyColour.orange:
+                _sr.sprite = orange;
+                colorText = "orange";
+                break;
+            case JellyColour.yellow:
+                _sr.sprite = yellow;
+                colorText = "yellow";
+                break;
+            default:
+                _sr.sprite = pink;
+                colorText = "something";
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _gb = other.gameObject;
+        StartCoroutine("ChangeColor");
+
+    }
+
+    private IEnumerator ChangeColor(){
+        yield return new WaitForSeconds(0.025f);
+        _gb.GetComponentInChildren<BakedAnimator>().ChangeColor(colorText);
+        StopCoroutine("ChangeColor");
+    }
+}
