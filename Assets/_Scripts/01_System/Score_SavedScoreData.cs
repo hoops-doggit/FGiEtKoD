8927fd5042a788054b1a9428590782c5f3bfb921
Unknown <jabcog@hotmail.com>
2018-12-19 13:59:11 +1300

    Committing Old Changes

--- Assets/_Scripts/01_System/Score_SavedScoreData.cs
+++ Assets/_Scripts/01_System/Score_SavedScoreData.cs
@@ -4,14 +4,14 @@
 
 public class Score_SavedScoreData {
 
-    public string name1 = "U_J"; public int score1 = 2900;
-    public string name2 = "mlem"; public int score2 = 999;
-    public string name3 = "sam"; public int score3 = 67;
+    public string name1 = "U_J"; public int score1 = 6648;
+    public string name2 = "mlem"; public int score2 = 6348;
+    public string name3 = "sam"; public int score3 = 5592;
     public string name4 = "Matt"; public int score4 = 2000;
-    public string name5 = "JAG"; public int score5 = 250;
-    public string name6 = "Veevee"; public int score6 = 1680;
+    public string name5 = "JAG"; public int score5 = 2250;
+    public string name6 = "Veevee"; public int score6 = 5680;
     public string name7 = "horse"; public int score7 = 1350;
-    public string name8 = "allan"; public int score8 = 720;
+    public string name8 = "allan"; public int score8 = 3320;
     public string name9 = "Ben"; public int score9 = 1357;
-    public string name10 = "VV"; public int score10 = 1680;
+    public string name10 = "VV"; public int score10 = 4543;
 }
