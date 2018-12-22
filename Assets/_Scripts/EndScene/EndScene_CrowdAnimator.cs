9c44d2ec83a26491cd0443af32d84d667fdef488
Jacob <jabcog@hotmail.com>
2018-12-23 09:56:55 +1300

    gummy crowd random rotation

--- Assets/_Scripts/EndScene/EndScene_CrowdAnimator.cs
+++ Assets/_Scripts/EndScene/EndScene_CrowdAnimator.cs
@@ -4,23 +4,20 @@ public class EndScene_CrowdAnimator : MonoBehaviour
 {
 
     private Animator _an;
-    public Transform _gbTransform;
     private SpriteRenderer _sr;
-
+    public float range;
 
 
     private void OnEnable()
     {
-
+        Vector3 gbt = gameObject.transform.rotation.eulerAngles;
+        float adjust = Random.Range(gbt.y - range, gbt.y + range);
         _an = GetComponentInChildren<Animator>();
         _an.speed = Random.Range(0.4f, 1.2f);
         _an.Play("CrowdJump01", 0, Random.Range(0f, 1f));
 
-    }
+        gameObject.transform.eulerAngles = new Vector3(gbt.x, adjust, gbt.z);
 
-    private void Update()
-    {
-        //transform.LookAt(_gbTransform);   
     }
 
 }
