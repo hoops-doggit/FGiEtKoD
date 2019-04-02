c3ed85f701da1c299fbf950c7a8986699d0bdcf9
Jacob <jabcog@hotmail.com>
2019-04-02 15:32:50 +1300

    Analytics01

---
+++ Assets/_Scripts/Analytics/EndOfLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class EndOfLevel : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            string seconds = _GM.instance.secondsElapsed.text;
+            string milliseconds = _GM.instance.millisecondsElapsed.text;
+            string time = seconds + "." + milliseconds;
+            OnGameOver(time, _GM.instance.numberOfJelliesCollected);
+            Debug.Log("PRintinGSTUFF" + time + _GM.instance.numberOfJelliesCollected);
+        }
+    }
+
+    public void OnGameOver(string time, int jelliesCollected)
+    {
+        Analytics.CustomEvent("reachedEnd", new Dictionary<string, object>
+        {
+            { "time", time },
+            { "jelliesCollected", jelliesCollected }
+        });
+    }
+}
