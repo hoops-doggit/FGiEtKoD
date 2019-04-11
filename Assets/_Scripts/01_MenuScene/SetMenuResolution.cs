c600796efb1f675f359809d39ea017bff5bc42e6
Jacob <jabcog@hotmail.com>
2019-04-12 10:23:54 +1200

    Created UI for new name entry box

---
+++ Assets/_Scripts/01_MenuScene/SetMenuResolution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMenuResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        int width = 320; // or something else
+        int height = 640; // or something else
+        bool isFullScreen = false; // should be windowed to run in arbitrary resolution
+        int desiredFPS = 60; // or something else
+
+        Screen.SetResolution(width, height, isFullScreen, desiredFPS);
+    }
+}
