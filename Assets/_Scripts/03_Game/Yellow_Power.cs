4c9018bb520ea4a4f556b548fc24c9e27c86a9a0
Jacob <jabcog@hotmail.com>
2019-05-15 11:53:47 +1200

    Lightning

---
+++ Assets/_Scripts/03_Game/Yellow_Power.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Yellow_Power : MonoBehaviour {
+
+    public GameObject lightning;
+    private GameObject lightningClone;
+    private Collider col;
+    public List<GameObject> go = new List<GameObject>();
+    public float lightningZOffset;
+   
+    public void LemonPower()
+    {
+        Vector3 position = col.gameObject.transform.position;
+        col.gameObject.transform.position = new Vector3(0, position.y, position.z + lightningZOffset);
+        col.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        go.Add(other.gameObject);
+    }
+    public void GetObjectsInCol(GameObject thing)
+    {
+        if(thing.tag == "jellybean")
+        {
+            //turn jelly yellow
+        }
+
+        else if(thing.tag == "pepper")
+        {
+            //make it smoke or destroyed// clense it of colour?
+        }
+    }
+    
+}
--- Assets/_Scripts/04_Kitchen/PepperShaker.cs
+++ Assets/_Scripts/04_Kitchen/PepperShaker.cs
@@ -51,10 +51,6 @@ public class PepperShaker : MonoBehaviour {
                 orange.transform.position = new Vector3(transform.position.x, 0, transform.position.z + orangeOffset);
 
                 Orange_Behaviour ob = orange.GetComponent<Orange_Behaviour>();
-                //ParticleSystem particles = Instantiate(ob.orangeDestroyedFX, gameObject.transform);
-                //particles.transform.parent = null;
-                //particles.transform.localScale = Vector3.one;
-
                 gotHit = true;
                 animating = true;
                 anim.SetTrigger("jump");
