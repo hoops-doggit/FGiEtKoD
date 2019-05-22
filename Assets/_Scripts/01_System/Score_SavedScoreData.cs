4152333609be757766efd41952443c4fae0719ba
Jacob <jabcog@hotmail.com>
2019-05-23 11:01:18 +1200

    Scores Updated

--- Assets/_Scripts/01_System/Score_SavedScoreData.cs
+++ Assets/_Scripts/01_System/Score_SavedScoreData.cs
@@ -4,15 +4,15 @@
 
 public class Score_SavedScoreData {
 
-    public string name1 = "U_J"; public int score1 = 2387;
-    public string name2 = "mlem"; public int score2 = 6348;
-    public string name3 = "sam"; public int score3 = 5592;
-    public string name4 = "Matt"; public int score4 = 2000;
-    public string name5 = "JAG"; public int score5 = 2250;
-    public string name6 = "Veevee"; public int score6 = 5680;
-    public string name7 = "horse"; public int score7 = 1350;
-    public string name8 = "allan"; public int score8 = 3320;
-    public string name9 = "Ben"; public int score9 = 1357;
+    public string name1 = "JACOB!"; public int score1 = 15152;
+    public string name2 = "slinky"; public int score2 = 13350;
+    public string name3 = "hehesia"; public int score3 = 15481;
+    public string name4 = "slinky"; public int score4 = -164363;
+    public string name5 = "Ariel"; public int score5 = 10506;
+    public string name6 = "Aiden"; public int score6 = 8202;
+    public string name7 = "Sean!!"; public int score7 = 6730;
+    public string name8 = "thatnzguy"; public int score8 = 9041;
+    public string name9 = "akamarak"; public int score9 = 11169;
     public string name10 = "VV"; public int score10 = 4543;
     public string playerName = "temp"; public int playerScore = 0;
 }
